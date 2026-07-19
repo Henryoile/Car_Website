ded43720038b9f3bc3ab2eb7f030f9d77abb89ff
Henryoile <henryoile627@gmail.com>
2026-07-19 17:57:24 +0300

    Add CarCard component and fetchCars utility, fix key prop warning

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
 import Image from "next/image";
 
-import { CustomFilter, Hero, SearchBar } from "@/components";
+import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
 
-export default function Home() {
+import { fetchCars } from "@/utils";
+
+export default async function Home() {
+  const allCars = await fetchCars();
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars
+  
   return (
     <main
       className="overflow-hidden">
@@ -18,16 +24,29 @@ export default function Home() {
             <SearchBar />
 
             <div className="home__filter-container">
-              <CustomFilter
-                title="fuel"
+              <CustomFilter title="fuel"
                 containerStyles="bg-primary-blue text-white rounded-full text-sm"
               />
-              <CustomFilter
-                title="year"
+              <CustomFilter title="year"
                 containerStyles="bg-white text-primary-blue rounded-full text-sm border border-primary-blue"
               />
             </div>
           </div>
+
+          {!isDataEmpty ? (
+            <section>
+              <div className="home__cars-wrapper">
+                {allCars?.map((car) => (
+                  <CarCard car={car} />
+                ))}
+              </div>
+            </section>
+          ) : (
+            <div className="home__error-container">
+              <h2 className="text-black text-xl font-bold">Oops, no cars found</h2>
+              <p>{allCars?.message}</p>
+            </div>
+          )}
 
         </div>
     </main>
